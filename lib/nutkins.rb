b7d07d6da2eb708401a660e6930d2b392b0b5575
James Pike <jpike@chilon.net>
2016-08-27 18:55:26 +0100

    patch Hash.dig for old ruby

--- lib/nutkins.rb
+++ lib/nutkins.rb
@@ -13,6 +13,21 @@ require "nutkins/docker_builder"
 require "nutkins/download"
 require "nutkins/version"
 
+unless Hash.new.respond_to? :dig
+  class Hash
+    def dig first_key, *other_keys
+      val = self[first_key]
+      if other_keys.empty?
+        val
+      elsif val and val.kind_of? Hash
+        val.dig *other_keys
+      else
+        nil
+      end
+    end
+  end
+end
+
 # Must be somedomain.net instead of somedomain.net/, otherwise, it will throw exception.
 module Nutkins
   CONFIG_FILE_NAME = 'nutkins.yaml'
